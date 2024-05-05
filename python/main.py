5c1b9e07f11ea470ddda03d1d060d8ecb9ee31a3
jellyfishsh <hikurage@protonmail.com>
2024-05-05 15:14:01 -0700

    Fixed main

--- python/main.py
+++ python/main.py
@@ -1,28 +1,20 @@
-import discord
-import commands as cmds
+import os
+import dotenv
 from dotenv import load_dotenv
-
+from pathlib import Path
 dotenv_path = Path('../.env')
 load_dotenv(dotenv_path=dotenv_path)
+TOKEN=os.getenv("TOKEN")
 
-
-class Atolla(discord.Client):
-    #Put all on_ready functions here
-    async def on_ready(self):
-        await bot.tree.sync()
-
-    #Put all on_message functions here
-    async def on_message(self, message):
-
-
-
-
-
+import discord
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+client = discord.Client(intents=intents)
 
-#probably prompt the user on docker install to put the ApplicationToken on first install
-client.run()
+@client.event
+async def on_ready():
+    print(f'Whe have logged in as {client.user}')
+
+client.run(TOKEN)
